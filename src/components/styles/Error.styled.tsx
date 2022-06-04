b83bd194729ef4bd352b138daedba1f723e49812
ryogazz <josuex.x.x@hotmail.com>
2022-06-04 20:13:43 -0300

    feat: implementação da rota + mensagem de erro

---
+++ src/components/styles/Error.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Error = styled.div`
+    background-color: rgba(255, 0, 0, 0.5);
+    color: #fff;
+    border-radius: 5px;
+    border: 1px solid #f00;
+    padding: 10px;
+`;
+
+export default Error;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { login } from '../../utils/wapAPI';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Container from '../../components/styles/Container.styled';
+import Error from '../../components/styles/Error.styled';
 
 const Display = styled.div`
   display: flex;
@@ -18,15 +20,22 @@ const Display = styled.div`
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const [isDisabled, setIsDisabled] = useState(true);
 
   useEffect(() => {
     setIsDisabled(!(username && password));
   }, [username, password]);
 
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    login(username, password).then((data) => {
-      console.log(data);
+    login(username, password).then(({ data }) => {
+      localStorage.setItem('token', data.token);
+      navigate('/home');
+    }).catch((err) => {
+      setError(err.message);
+      alert(err.message);
     });
   };
 
@@ -45,6 +54,11 @@ function Login() {
           value={password}
           onChange={({ target }) => setPassword(target.value)}
         />
+        {
+          error && (
+            <Error>{error}</Error>
+          )
+        }
         <Button disabled={isDisabled} onClick={handleClick} />
       </Container>
     </Display>
